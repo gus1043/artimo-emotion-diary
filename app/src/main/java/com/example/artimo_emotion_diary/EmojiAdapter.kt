50d5444d90a2bb9d9ad89611cd9b0d7eaedfb246
JihyeonChoi <gus1043@gmail.com>
2024-08-21 15:40:33 +0900

    style : emoji 가운데 위치

--- app/src/main/java/com/example/artimo_emotion_diary/EmojiAdapter.kt
+++ app/src/main/java/com/example/artimo_emotion_diary/EmojiAdapter.kt
@@ -35,12 +35,12 @@ class EmojiAdapter(private val context: Context, private val imageList: List<Str
 
             // 이미지를 클릭할 때의 동작 설정
             holder.imageView.setOnClickListener {
-//                val intent = Intent(context, FullScreenImageActivity::class.java)
-//
-//                // 이미지 파일 이름을 전달
-//                intent.putExtra("imageFileName", imageFileName)
-//
-//                context.startActivity(intent)
+                val intent = Intent(context, WriteActivity::class.java)
+
+                // 이미지 파일 이름을 전달
+                intent.putExtra("emoji", imageFileName)
+
+                context.startActivity(intent)
             }
 
         } catch (e: IOException) {
@@ -56,45 +56,3 @@ class EmojiAdapter(private val context: Context, private val imageList: List<Str
         val imageView: ImageView = itemView.findViewById(R.id.imageView)
     }
 }
-
-
-
-
-
-
-//class EmojiAdapter(private val context: Context, private val imageList: List<String>) :
-//    RecyclerView.Adapter<EmojiAdapter.ImageViewHolder>() {
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
-//        Log.d("EmojiAdapter", "onCreateViewHolder called")
-//        val view = LayoutInflater.from(context).inflate(R.layout.item_emoji, parent, false)
-//        return ImageViewHolder(view)
-//    }
-//
-//    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
-//        val imageFileName = imageList[position]
-//        Log.d("EmojiAdapterBind", "Binding image: $imageFileName at position: $position")
-//        try {
-//            val assetManager: AssetManager = context.assets
-//            val inputStream = assetManager.open("emoji/$imageFileName")
-//            val drawable = Drawable.createFromStream(inputStream, null)
-//            holder.imageView.setImageDrawable(drawable)
-//            inputStream.close()
-//            Log.d("EmojiAdapterBind", "Successfully loaded image: $imageFileName")
-//        } catch (e: IOException) {
-//            Log.e("EmojiAdapterBind", "Error loading image: $imageFileName", e)
-//        }
-//    }
-//
-//    override fun getItemCount(): Int {
-//        val count = imageList.size
-//        Log.d("EmojiAdapter7", "Item count: $count")
-//        return count
-//    }
-//
-//    inner class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//        val imageView: ImageView = itemView.findViewById(R.id.imageView)
-//        init {
-//            Log.d("EmojiAdapter8", "ViewHolder initialized")
-//        }
-//    }
-//}
